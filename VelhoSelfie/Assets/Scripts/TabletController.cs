9dbd71434d7df298d4c9849214a6e5bd8a3a40ae
Biblioteca <Biblioteca@D_012993.demprel.local>
2024-10-19 22:14:14 -0300

    Tablet animation code

--- VelhoSelfie/Assets/Scripts/TabletController.cs
+++ VelhoSelfie/Assets/Scripts/TabletController.cs
@@ -1,21 +1,42 @@
+using PrimeTween;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
+[RequireComponent(typeof(StateTokenComponent))]
 public class TabletController : MonoBehaviour
 {
+    [SerializeField] RectTransform cameraTexture;
     [SerializeField] RectTransform restTransform;
+    [SerializeField] RectTransform focusTransform;
+    [SerializeField] TweenSettings<float> tweenSettings;
+ 
+    CancellationToken AliveToken => GetComponent<StateTokenComponent>().AliveToken;
+    Tween tween;
+
+    void OnTween(float t)
+    {
+        cameraTexture.transform.localPosition = Vector3.Lerp(restTransform.localPosition, focusTransform.localPosition, t);
+        cameraTexture.transform.localRotation = Quaternion.Slerp(restTransform.localRotation, focusTransform.localRotation, t);
+    }
+
     void OnPlayerLookedDown(bool lookedDown)
     {
         if (lookedDown)
         {
-
+            tweenSettings.startValue = tween.isAlive ? tween.progress : 0f;
+        } else
+        {
+            tweenSettings.startValue = tween.isAlive ? tween.progress : 1f;
         }
+        tween.Stop();
+        tween = Tween.Custom(tweenSettings, OnTween);
     }
 
     void Start()
     {
-        
+        GameEvents.PlayerIsLookingDown.AddListener(OnPlayerLookedDown, AliveToken);
     }
 
 }

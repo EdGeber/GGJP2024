d000612444b4eb503001c33bdbee43dc16abe6b6
Biblioteca <Biblioteca@D_012993.demprel.local>
2024-10-20 17:12:15 -0300

    Fix Level0 and Level1

--- VelhoSelfie/Assets/Scripts/Level1.cs
+++ VelhoSelfie/Assets/Scripts/Level1.cs
@@ -2,17 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Level1 : MonoBehaviour
+public class Level1 : Level
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public override int LevelNumber => 1;
 }
